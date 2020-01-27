ade11a3978c5904dc033f56b9e6988215fce5881
T7Rze <59921402+Tom7Rze@users.noreply.github.com>
2020-01-27 15:39:49 +0100

    Tutaj napisane są funkcje różnego rodzaju
    Także zawarte przekazywanie danych metodą post

---
+++ funkcjezabpost.php
@@ -0,0 +1,22 @@
+﻿
+
+<form method="post" action="funkcjezabpost.php">
+   Predkosc (km/h):   <input type="text" name="predkosc" /><br />
+   Odległość(km):  <input type="text" name="odleglosc" /><br />
+  <input type="submit" value="Policz">
+</form>
+
+
+
+
+
+<?php
+var_dump(substr('atyy', 2)); // bool(false)
+
+function czas()
+{ 
+	$czas=$_POST["odleglosc"]/$_POST["predkosc"];
+	return $czas;
+}
+echo "Czas przejazdu na trasie ".$_POST["odleglosc"]." km pojazdu jadącego z prędkością v = ".$_POST["predkosc"]. " km/h wynosi ".czas()." godzin.";
+?>
